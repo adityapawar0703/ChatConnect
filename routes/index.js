6945b64db1e0b9e30ad52b2ada7b4c2895d5a347
Aditya Pawar <adityapawar07m@gmail.com>
2025-07-10 22:58:56 +0530

    new

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", function (req, res) {
-  res.render("index");
+  res.render("test");
 });
 
 router.get("/test",function(req,res){
